fb4042dddad5b9db5468efc706a55d030b47c741
ZhaoLiangzhe <22302010032@m.fudan.edu.cn>
2024-12-06 19:39:41 +0800

    ODConvnext

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -999,7 +999,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             SCDown,
             C2fCIB,
             ConvNextBlock,
-            ODConv,
+            ODConv_3rd,
         }:
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
@@ -1027,7 +1027,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
                 C2fCIB,
                 C2PSA,
                 ConvNextBlock,
-                ODConv,
+                ODConv_3rd,
             }:
                 args.insert(2, n)  # number of repeats
                 n = 1
